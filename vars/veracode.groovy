3929c52414785de947a4279308bd43c682952747
Stiven <barloff2@gmail.com>
2023-05-16 18:14:51 -0500

    Adicion veracode call

--- vars/veracode.groovy
+++ vars/veracode.groovy
@@ -2,7 +2,7 @@ import groovy.sql.Sql
 import static java.sql.ResultSet.*
 
 void call() {
-    def db = database(
+    def db = Database(
         connectionString: 'jdbc:sqlserver://172.17.0.3:1433;;databaseName=test_stiven;user=sa;password=Der3480*',
         driverName: 'com.microsoft.sqlserver.jdbc.SQLServerDriver'
     )
