598eff18a4cfbb2223f6d7b0bea95a8081b91b46
Stiven <barloff2@gmail.com>
2023-05-16 18:14:04 -0500

    Adicion veracode call

--- vars/veracode.groovy
+++ vars/veracode.groovy
@@ -1,14 +1,13 @@
 import groovy.sql.Sql
 import static java.sql.ResultSet.*
-import com.microsoft.sqlserver.jdbc.SQLServerDriver
 
 void call() {
-    def db = [
-        url: 'jdbc:sqlserver://172.17.0.3:1433;databaseName=test_stiven;user=sa;password=Der3480*',
-        user: 'sa',
-        password: 'Der3480*',
-        driver: 'com.microsoft.sqlserver.jdbc.SQLServerDriver'
-    ]
-    def sql = Sql.newInstance(db.url, db.user, db.password, db.driver)
-    sql.execute 'SELECT * FROM usuario'
+    def db = database(
+        connectionString: 'jdbc:sqlserver://172.17.0.3:1433;;databaseName=test_stiven;user=sa;password=Der3480*',
+        driverName: 'com.microsoft.sqlserver.jdbc.SQLServerDriver'
+    )
+    def result = db.read([
+        sql: 'SELECT * FROM usuario',
+        returnRows: 10
+    ])
 }
