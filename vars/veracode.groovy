13f60022f4ce5871b64be588dafaf6b07ea4ec6e
Stiven <barloff2@gmail.com>
2023-05-17 15:20:42 -0500

    actualización conexión bd credentials

--- vars/veracode.groovy
+++ vars/veracode.groovy
@@ -6,7 +6,8 @@ void call() {
     withCredentials([usernamePassword(credentialsId: 'DatabaseVeracode', passwordVariable: 'SQLSERVER_PWD', usernameVariable: 'SQLSERVER_USER')]) {
         sh(script:"""
             export PATH=\$PATH:/opt/mssql-tools/bin
-            sqlcmd -S 172.17.0.2 -d test_stiven -U ${SQLSERVER_USER} -P ${SQLSERVER_PWD} -Q \"SELECT * FROM usuario;\"
+            sqlcmd -S 172.17.0.2 -d test_stiven -U ${SQLSERVER_USER} -P ${SQLSERVER_PWD} -Q \"SELECT * FROM usuario;\" -o ./output.txt
+            cat ./output.txt
         """)
     }
 
