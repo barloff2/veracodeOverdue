172b62201709ab00d07edb5b252f851d11565f61
Stiven <barloff2@gmail.com>
2023-05-17 10:12:23 -0500

    actualización conexión bd

--- vars/veracode.groovy
+++ vars/veracode.groovy
@@ -1,25 +1,29 @@
 import groovy.sql.Sql
 import static java.sql.ResultSet.*
+import java.sql.DriverManager
 
 void call() {
-    /*def db = Database(
-        connectionString: 'jdbc:sqlserver://172.17.0.3:1433;;databaseName=test_stiven;user=sa;password=Der3480*',
-        driverName: 'com.microsoft.sqlserver.jdbc.SQLServerDriver'
-    )
-    def result = db.read([
-        sql: 'SELECT * FROM usuario',
-        returnRows: 10
-    ])*/
-
     String query = "SELECT * FROM usuario"
-    def driver = "com.microsoft.sqlserver.jdbc.SQLServerDriver"
+    //def driver = "com.microsoft.sqlserver.jdbc.SQLServerDriver"
     String username =  'sa'
     String password = 'Der3480*'
     String url = "jdbc:sqlserver://172.17.0.3:1433;databaseName=test_stiven"
+    def classLoader = this.class.classLoader
+    while (classLoader.parent) {
+        classLoader = classLoader.parent
+        if (classLoader.getClass() == java.net.URLClassLoader) {
+            classLoader.addURL(new File("${JDBC_SQLSERVER}").toURI().toURL)
+            break;
+        }
+    }
     
-    def sql = Sql.newInstance(url, username, password, driver)
+    Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver")
+    con = DriverManager.getConnection(url, username, password)
+    stmt = con.createStatment()
+    
+    /*def sql = Sql.newInstance(url, username, password, driver)
 
     def result = sql.rows(query)
 
-    sql.close()
+    sql.close()*/
 }
